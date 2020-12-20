c951129c45c561a505cb4ddf00c6e89410e6cb65
eoohosun <redysj0517@naver.com>
2020-12-20 20:18:14 +0900

    a

--- node-study/app.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const createUserSchema = () => {
-    UserSchema = require('./database/user-schema').createSchema(mongoose);
-    const UserModel = mongoose.model('user3', UserSchema);
-    console.log('UserModel 정의');
-};
