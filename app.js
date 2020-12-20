6d56fe0c48d3024ed7b33e5f41812e69f0c304f4
eoohosun <redysj0517@naver.com>
2020-12-21 01:50:25 +0900

    init setting coding

--- app.js
+++ app.js
@@ -1,7 +1,78 @@
+/**
+===================================================================
+========================== require start ==========================
+===================================================================
+*/
+const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressSession = require('express-session');
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+const path = require('path');
 
-const createUserSchema = () => {
-    UserSchema = require('./database/user-schema').createSchema(mongoose);
-    const UserModel = mongoose.model('user3', UserSchema);
-    console.log('UserModel 정의');
+/**
+===================================================================
+========================== require end ============================
+===================================================================
+*/
+
+/**
+===================================================================
+========================== setting start ==========================
+===================================================================
+*/
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/**
+===================================================================
+========================== setting end ============================
+===================================================================
+*/
+/**
+===================================================================
+========================== database start =========================
+===================================================================
+*/
+const database;
+const UserSchema;
+const UserModel;
+
+const DB = {
+    database:{},
+    createUserSchema() {
+        UserSchema = require('./database/user-schema').createSchema();
+        const UserModel = mongoose.model('user3', UserSchema);
+        console.log('UserModel 정의');
+    },
+    connectDB() {
+        const dbUrl = 'mongodb://localhost:27017/shopping';
+ 
+        mongoose.connect(dbUrl);
+        this.database = mongoose.connection;
+        this.database.on('error', console.error.bind(console,'mongoose connection error'));
+        this.database.on('open', () => {
+            console.log(`database connect success ${dbUrl}`);
+            this.createUserSchema();
+        });        
+    },
 };
+
+DB.connectDB();
+
+/**
+===================================================================
+========================== database end ===========================
+===================================================================
+*/
+
+module.exports = app;
